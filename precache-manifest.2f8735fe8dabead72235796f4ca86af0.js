21bf5d8d08b5c1723fd050254894f20e13bf6866
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-11-21 20:38:03 -0800

    Updates

--- precache-manifest.2f8735fe8dabead72235796f4ca86af0.js
+++ precache-manifest.2f8735fe8dabead72235796f4ca86af0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45a823301fe06b32f5628cd00686d729",
+    "revision": "15526fc77b847c36fd11b34ea3e27bbf",
     "url": "/index.html"
   },
   {
-    "revision": "c39cd619c628122a084d",
+    "revision": "b7457b6be628bf79482c",
     "url": "/static/css/main.5be9e5db.chunk.css"
   },
   {
-    "revision": "cc92b38c2f23a58802fc",
-    "url": "/static/js/2.c04a1e82.chunk.js"
+    "revision": "e605ca878f889c5f76cb",
+    "url": "/static/js/2.e424187e.chunk.js"
   },
   {
     "revision": "0121e4c97502d1688ffcc994b143cb63",
-    "url": "/static/js/2.c04a1e82.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e424187e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c39cd619c628122a084d",
-    "url": "/static/js/main.0491ccaa.chunk.js"
+    "revision": "b7457b6be628bf79482c",
+    "url": "/static/js/main.ba2e6035.chunk.js"
   },
   {
     "revision": "2747b914e9a60db2276f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25faba5b8abcb15983210fd7e10f3add.js"
+  "/precache-manifest.2f8735fe8dabead72235796f4ca86af0.js"
 );
 
 self.addEventListener('message', (event) => {
